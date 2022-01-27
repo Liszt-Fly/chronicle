641b5c6d925772a01cbf7f4d44f40ddf755be378
Liszt-Fly <1904231450@qq.com>
2022-01-27 22:15:03 +0800

    1.0

--- src/composables/format.ts
+++ src/composables/format.ts
@@ -1,14 +1,16 @@
 
 import parserBabel from "prettier/esm/parser-babel.mjs"
 import prettier from 'prettier/esm/standalone.mjs'
-
+import phpPlugin from "@prettier/plugin-php/standalone";
 //sum formatter
 let parseSet = new Map()
 parseSet.set("js", parserBabel)
 
 export function format(source: String, language: string) {
 	console.log(`language${language}`)
-	return prettier.format(source, {
+    return prettier.format(source, {
+        // parser:"php",
+        // plugins: [phpPlugin],
 		parser: "babel",
 
 		plugins: [parseSet.get(language)],
