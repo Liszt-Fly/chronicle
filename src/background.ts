64f6b70d5a59d48c338807b523d702de4dfefe6e
forestlet <hyf2021@outlook.com>
2022-01-28 11:16:27 +0800

    refine

--- src/background.ts
+++ src/background.ts
@@ -15,8 +15,9 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
 	// Create the browser window.
 	const win = new BrowserWindow({
-		width: 1200,
-		height: 800,
+		width: 800,
+		height: 600,
+		transparent: true,
 		webPreferences: {
 			// Use pluginOptions.nodeIntegration, leave this alone
 			// See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
