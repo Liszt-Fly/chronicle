70768c93d36e494ee0d24d8711a5a35726f4c448
forestlet <hyf2021@outlook.com>
2022-01-28 10:38:21 +0800

    update router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue"
 import App from "./App.vue"
-import router from "./router/index"
+import router from "./router/router"
 import paragraph from "@/components/CDOM/paragraph.vue"
 import codeBlock from "@/components/CDOM/codeBlock.vue"
 
@@ -9,6 +9,7 @@ import "./style/light.scss"
 import "bootstrap-icons/font/bootstrap-icons.css"
 
 const app = createApp(App)
+
 app.component("paragraph", paragraph)
 app.component("codeBlock", codeBlock)
 app.use(router).mount("#app")
