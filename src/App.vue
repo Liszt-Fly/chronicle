1fa51e8ba90fecc3f5cd911fe3355f58d38736a7
forestlet <hyf2021@outlook.com>
2022-01-28 15:16:33 +0800

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import TabBar from './views/TabBar.vue'
-import Editor from './views/Main/Editor.vue';
+import TabBar from '@/views/TabBar.vue'
+import Editor from '@/views/Main/Editor.vue';
 </script>
 
 <template>
