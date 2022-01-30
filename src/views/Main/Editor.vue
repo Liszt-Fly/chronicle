3f5b5389387a68ec0d280b43f4fc4a1a367ef81b
forestlet <hyf2021@outlook.com>
2022-01-30 19:52:23 +0800

    fix editable

--- src/views/Main/Editor.vue
+++ src/views/Main/Editor.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted, ref, watchEffect } from "vue"
+import { onMounted, Ref, ref, watchEffect } from "vue"
 import { currentFile, paragraphs } from "@/api/configdb"
 import { initMarked } from "@/api/init"
 import { loadNodeLists, saveArticle } from "@/api/Editor/Editor"
@@ -24,6 +24,11 @@ onMounted(() => {
 	})
 })
 
+let editable: Ref<boolean> = ref(false)
+let edit = function () {
+	editable.value = !editable.value
+}
+
 </script>
 
 <template>
@@ -31,7 +36,17 @@ onMounted(() => {
 		<div class="extendedPanel">
 			<FileSystem></FileSystem>
 		</div>
-		<div class="editor" ref="rContainer" @keydown="save($event)">
+		<div class="magic" @click="edit()" title="点击改变选择模式">
+			<i
+				:class="[
+					'bi',
+					{ 'bi-pencil-square': editable },
+					{ 'bi-eye': !editable },
+				]"
+			></i>
+		</div>
+
+		<div class="editor" ref="rContainer" @keydown="save($event)" :contenteditable="editable">
 			<template v-for="paragraph in paragraphs" :key="paragraph.title">
 				<component :is="paragraph.type" :paragraph="paragraph"></component>
 			</template>
@@ -39,3 +54,20 @@ onMounted(() => {
 	</div>
 </template>
 
+<style>
+.magic {
+	position: fixed;
+	top: 20px;
+	right: 20px;
+	width: 32px;
+	height: 32px;
+	text-align: center;
+	background-color: #fff;
+	border-radius: 5px;
+	display: grid;
+	color: #444;
+	justify-items: center;
+	align-items: center;
+	cursor: pointer;
+}
+</style>
