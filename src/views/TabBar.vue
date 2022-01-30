dd724e4ebf7c54ec74ea21ab5292b342a8423fcf
forestlet <hyf2021@outlook.com>
2022-01-30 17:50:26 +0800

    refine tab bar

--- src/views/TabBar.vue
+++ src/views/TabBar.vue
@@ -17,23 +17,36 @@ function openRepository() {
 
 function empty() { }
 
-let tabBarItems = [
-	{ item: "folder", icon: "bi bi-archive", func: openRepository, to: "/editor", },
-	{ item: "search", icon: "bi bi-search", func: empty, to: "/search" },
-	{ item: "tag", icon: "bi bi-bookmark-heart", func: empty, to: "/tag" },
-	{ item: "timeline", icon: "bi bi-calendar-event", func: empty, to: "/timeline" },
-	{ item: "setting", icon: "bi bi-sliders", func: empty, to: "/setting" },
-]
 </script>
 
 <template>
 	<div class="tab-system">
-		<div class="tab-bar" v-if="display">
-			<template v-for="tabItem in tabBarItems">
-				<div class="tab-item" @click="tabItem.func()">
-					<router-link :class="tabItem.icon" :to="tabItem.to"></router-link>
-				</div>
-			</template>
+		<div class="tab-bar">
+			<div class="tab-item" @click="openRepository" id="folder">
+				<router-link to="/editor">
+					<i class="bi bi-archive"></i>
+				</router-link>
+			</div>
+			<div class="tab-item" @click="empty" id="search">
+				<router-link to="/search">
+					<i class="bi bi-search"></i>
+				</router-link>
+			</div>
+			<div class="tab-item" @click="empty" id="tag">
+				<router-link to="/tag">
+					<i class="bi bi-bookmark-heart"></i>
+				</router-link>
+			</div>
+			<div class="tab-item" @click="empty" id="timeline">
+				<router-link to="/timeline">
+					<i class="bi bi-calendar-event"></i>
+				</router-link>
+			</div>
+			<div class="tab-item" @click="empty" id="setting">
+				<router-link to="/setting">
+					<i class="bi bi-gear-wide-connected"></i>
+				</router-link>
+			</div>
 		</div>
 	</div>
 </template>
