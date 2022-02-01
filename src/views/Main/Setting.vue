83d90d7a482fa8514b193bf2eb45dd059cf7dc13
forestlet <hyf2021@outlook.com>
2022-02-01 11:51:01 +0800

    update Setting page

--- src/views/Main/Setting.vue
+++ src/views/Main/Setting.vue
@@ -1,22 +1,15 @@
-<template>setting page</template>
+<template>
+    <el-row>
+        <el-col :span="24">
+            <el-checkbox v-model="welcome_switch" label="展开欢迎页面"></el-checkbox>
+        </el-col>
+    </el-row>
+</template>
 
-<script lang="ts">
-export default {
-    data() {
-        return {
-        }
-    },
-    computed: {
-    },
-    watch: {
+<script lang="ts" setup>
+import { ref } from 'vue'
 
-    },
-    methods: {
-
-    },
-    mounted() {
-    }
-}
+let welcome_switch = ref(true)
 </script>
 
 <style >
