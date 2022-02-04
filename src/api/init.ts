effb52a150e82edd7ceb9febc9f62c3d2ac75fc4
forestlet <hyf2021@outlook.com>
2022-02-04 22:06:35 +0800

    update Setting

--- src/api/init.ts
+++ src/api/init.ts
@@ -10,7 +10,7 @@ const configFileData = fs.readFileSync(configFile)
 let config = JSON.parse(configFileData)
 
 // 样式初始化
-export let addStyle = () => {
+let initTheme = () => {
 	let global_theme = config.global_theme
 	let editor_theme = config.editor_theme
 	let code_theme = config.code_theme
@@ -32,4 +32,28 @@ export let addStyle = () => {
 	let codeStyle = document.createElement('style');
 	codeStyle.innerText = fs.readFileSync(code_theme_path)
 	head.appendChild(codeStyle);
+}
+
+// 字体初始化
+let initFont = () => {
+	let global_font = config.global_font
+	let code_font = config.code_font
+
+	const head = document.head || document.getElementsByTagName('head')[0];
+
+	let fontStyle = document.createElement('style');
+	fontStyle.innerText = `
+	body {
+		font-family: ${global_font};
+	}
+	.cm-editor .cm-content {
+		font-family: ${code_font};
+	}`
+	head.appendChild(fontStyle);
+}
+
+// 初始化设置
+export let initSetting = () => {
+	initTheme()
+	initFont()
 }
