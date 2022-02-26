5e11c085b6bbaa0a4b0f4ab2d0bdc051b2443eee
forestlet <hyf2021@outlook.com>
2022-02-26 13:17:57 +0800

    update

--- src/api/router/router.ts
+++ src/api/router/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router"
-import Editor from "@/views/Editor.vue"
-import Setting from "@/views/Setting.vue"
-import Stream from "@/views/Stream.vue"
+import Editor from "@/views/Main/Editor.vue"
+import Setting from "@/views/Main/Setting.vue"
+import Stream from "@/views/Main/Stream.vue"
 const router = createRouter({
 
 	history: createWebHashHistory(),
