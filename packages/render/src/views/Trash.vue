d678e6d8b65ee2981f030f77264630daba5848a7
Liszt-Fly <1904231450@qq.com>
2022-06-22 14:16:36 +0800

    update

--- packages/render/src/views/Trash.vue
+++ packages/render/src/views/Trash.vue
@@ -1,9 +1,22 @@
 <template>
     <div class="trash">
+        <el-row>
+            <div class="router">
+                <i class="bi bi-chevron-left" @click="restore"></i>
+                <i class="bi bi-chevron-right"></i>
+
+            </div>
+        </el-row>
+        <el-divider></el-divider>
         <el-row :gutter="20">
             <template v-for="item in container">
-                <el-col :span="6">
-                    <el-card>
+                <el-col :span="4">
+                    <div class="card" @click="expand(item)">
+                        <i :class="item.children ? 'bi bi-folder2' : 'bi bi-file-earmark-text'"></i>
+                        <div>{{ item.name }}</div>
+                    </div>
+
+                    <!-- <el-card>
                         <template #header class="header">
                             <div class="header"> <i class="bi bi-arrow-left" v-if="item.parent && !isTrashBinRoot(item)"
                                     @click="restore(item)"></i> {{ removeExtName(item.name) }} <i
@@ -17,7 +30,7 @@
 
                             <li>进入回收站时间: 2022/2/13</li>
                         </ul>
-                    </el-card>
+                    </el-card> -->
                 </el-col>
             </template>
         </el-row>
@@ -25,48 +38,64 @@
 </template>
 
 <script lang="ts" setup>
-import { fileNode } from '@/api/FileTree/fileNode';
-import { chronicleUserPath } from '@/api/init';
-import { trashBin } from '@/data/configdb';
-import path from "path"
-import { removeExtName } from "@/api/FileSystem/filesystem"
-import { onMounted, Ref, ref, watchEffect } from 'vue';
-import TrashItem from '../components/Trash/TrashItem.vue';
+import { fileNode } from "@/api/FileTree/fileNode";
+import { chronicleUserPath } from "@/api/init";
+import { trashBin } from "@/data/configdb";
+import path from "path";
+import { removeExtName } from "@/api/FileSystem/filesystem";
+import { onMounted, Ref, ref, watchEffect } from "vue";
+import TrashItem from "../components/Trash/TrashItem.vue";
+import { NodeType } from "@/api/FileTree/type";
+import { Stack } from "@/api/stack";
 //* 声明一个container容器，用于切换页面
-let container = ref(trashBin.value!.root.children!)
+let container: Ref<fileNode[]> = ref(trashBin.value!.root.children!);
+let stack = new Stack<fileNode[]>([])
+let stackRef = ref(stack)
+stackRef.value.push(container.value)
 const expand = (item: fileNode) => {
-    container.value = item.children!
-}
-
-
+    if (item.type == NodeType.FOLDER) {
+        container.value = item.children!;
+        stackRef.value.push(container.value)
+        console.log(stackRef.value.peek())
+        container.value = stackRef.value.peek()
+    }
+};
 
 const isTrashBinRoot = (item: fileNode) => {
-    return item.parent!.path == path.resolve(chronicleUserPath, ".trash")
-}
-const restore = (item: fileNode) => {
+    return item.parent!.path == path.resolve(chronicleUserPath, ".trash");
+};
+const restore = () => {
 
-    console.log(item.parent?.children)
-    container.value = item.parent!.parent!.children!
-}
+    console.log(stackRef.value.pop())
+
+    container.value = stackRef.value.peek()
+};
 watchEffect(() => {
-    console.log(trashBin.value)
-})
+    console.log(trashBin.value);
+});
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .trash {
     overflow: scroll;
     height: calc(100vh - var(--brand-height));
+    padding: 20px;
+    user-select: none;
 }
 
-.header {
+.card {
+    justify-content: center;
+    align-items: center;
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+    flex-direction: column;
 
     i {
         cursor: pointer;
+        font-size: 2rem;
     }
+}
 
+.router i {
+    margin-right: 20px;
 }
 </style>
