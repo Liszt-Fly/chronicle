d6fea8df00aef8d739b79cd744a693255445f875
forestlet <hyf2021@outlook.com>
2022-07-18 15:40:10 +0800

    初步实现 menu 组件

--- packages/render/src/components/FileSystem/FileSystem.vue
+++ packages/render/src/components/FileSystem/FileSystem.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import path from "path";
-import { bClickedParent, fTree, menuDisplay } from "@/data/configdb";
+import { bClickedParent, fTree, menuDisplay, currentFile, openFiles } from "@/data/configdb";
 import { chronicleUserPath } from "@/api/init";
 import FileList from "@/components/FileSystem/FileList.vue";
 import { fileTree } from "@/api/FileTree/fileTree";
+import { NodeType } from "@/api/FileTree/type";
 import { computed, onMounted, Ref, ref } from "vue";
 import { fileNode } from "@/api/FileTree/fileNode";
 import fsp from "fs-extra"
-import Menu from "@/components/FileSystem/Menu.vue";
+import Menu from "@/components/Menu/Menu.vue";
+import router from "@/router/router";
 let targetDom: Ref<null | HTMLElement> = ref(null);
 
 let menuX = ref(0);
@@ -37,14 +39,13 @@ const getTargetNode = (node: HTMLElement): HTMLElement => {
   else if (node.tagName == "DIV") {
     node = node.querySelector("span")!
   }
-  console.log(`处理后:${node.tagName}`)
+  // console.log(`处理后:${node.tagName}`)
   return node
 }
 
 let showMenu = (e: MouseEvent) => {
-
   targetDom.value = getTargetNode(e.target as HTMLElement)
-  console.log(targetDom.value)
+  // console.log(targetDom.value)
   bClickedParent.value = false;
   menuDisplay.value = "hidden";
   setTimeout(() => {
@@ -90,11 +91,68 @@ onMounted(() => {
     new fileNode(path.resolve(chronicleUserPath, "assets"), "assets")
   );
   if (!fileTree.currentFileNode) {
-
     // fileTree.currentFileNode = fTree.value.root.children![0]
   }
 });
+
+const remove = () => {
+  openFiles.value.delete(currentFile.value)
+  if (openFiles.value.size != 0) {
+    currentFile.value = Array.from(openFiles.value).pop()!;
+  } else {
+    currentFile.value = ""
+  }
+  fileTree.currentFileNode.removeSelf();
+  router.push("/Editor")
+};
+
+const addTags = () => {
+  // dialogVisible.value = true;
+};
+
+const rename = () => {
+  targetDom!.contentEditable = "true";
+  targetDom!.focus();
+  let range = new Range();
+  range.setStart(targetDom! as Node, 0);
+  range.setEnd(targetDom! as Node, 1);
+  document.getSelection()!.removeAllRanges();
+  document.getSelection()!.addRange(range);
+};
+
+const duplicate = () => {
+  fileTree.currentFileNode.duplicate();
+};
+
+const exportFile = () => { };
+
+const exportDir = () => { };
+
+const addChildren = (t: NodeType) => {
+  if (bClickedParent.value) {
+    fTree.value!.root.addChildren(t);
+  } else {
+    fileTree.currentFileNode.addChildren(t);
+  }
+};
+
+const addFoler = () => { addChildren(NodeType.FOLDER) };
+
+const addFile = () => { addChildren(NodeType.FILE) };
+
+let menu_items = [
+  { icon: "bi bi-file-earmark-plus", text: "add_File", func: addFile },
+  { icon: "bi bi-folder-plus", text: "add_File", func: addFoler },
+  { icon: "bi bi-files", text: "duplicate", func: duplicate },
+  { icon: "bi bi-reply", style: "transform: rotate(90deg)", text: "export_PDF", func: exportFile },
+  { icon: "bi bi-reply-all", text: "export_all", func: exportDir },
+  { icon: "bi bi-input-cursor", text: "rename", func: rename },
+  { icon: "bi bi-trash3", text: "move_to_trash", func: remove },
+  { icon: "bi bi-pin", text: "pin", func: addTags },
+  { icon: "bi bi-bookmarks", text: "add_tags", func: addTags }
+]
 </script>
+
 <template>
   <div class="file-system" ref="filesystem" @click="hideMenu" @contextmenu.capture="showParentMenu($event)"
     @dragover.prevent @drop="drop($event)">
@@ -108,6 +166,6 @@ onMounted(() => {
       display: menuDisplay,
       top: menuY + 'px',
       left: menuX + 'px',
-    }" @click="hideMenu()" :dom="targetDom"></Menu>
+    }" @click="hideMenu()" :menu_items="menu_items"></Menu>
   </div>
 </template>
---
+++ packages/render/src/components/Menu/Menu.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps({
+  menu_items: Array
+});
+
+// console.log(props.menu_items)
+
+</script>
+
+<template>
+  <div class="menu">
+    <template v-for="menu_item in menu_items">
+      <div class="text-item" @click="menu_item.func">
+        <i :class="menu_item.icon" :style="menu_item.style"></i>
+        <el-divider direction="vertical" />
+        <span>{{ $t(`editor.menu.${menu_item.text}`) }}</span>
+      </div>
+    </template>
+  </div>
+</template>
+
+<style scoped lang="scss">
+:root {
+  --el-transition-duration: 0;
+}
+
+.menu {
+  width: 160px;
+  position: fixed;
+  z-index: 999;
+  padding: 4px;
+  border-radius: 4px;
+  background-color: var(--el-bg-color);
+  box-shadow: var(--el-box-shadow-lighter);
+
+  .bi-reply::before,
+  .bi-reply-all::before {
+    transform: rotateY(180deg);
+  }
+
+  .el-divider--horizontal {
+    margin: 4px;
+    width: auto;
+  }
+
+  .text-item {
+    border-radius: 4px;
+    font-size: 0.8rem;
+    padding: 8px 12px;
+    text-align: left;
+
+    &:hover {
+      background-color: var(--el-color-info-light-9);
+      cursor: pointer;
+    }
+  }
+}
+</style>
